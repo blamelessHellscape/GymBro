7466ab97ee07215c8dfc8fa7ee2931bc26b0515b
sjh5888 <ginganinjarules@gmail.com>
2024-11-09 12:52:02 -0500

    v1.0

--- db_helper.py
+++ db_helper.py
@@ -2,74 +2,71 @@ import sqlite3
 import datetime
 import pandas as pd
 
+
 class Db_Helper:
     db = None
+
     def __init__(self):
         print('db init')
         self.db = sqlite3.connect('gains.db',  check_same_thread=False)
-        self.run_query('''create table if not exists exercise_table(id INTEGER PRIMARY KEY, row_id INTEGER, reps INTEGER, weight INTEGER, user TEXT, exercise TEXT)''')
-        self.run_query('''create table if not exists pr_table(id INTEGER PRIMARY KEY, date TEXT, max_weight INTEGER, user TEXT, exercise TEXT)''')
+        self.run_query(
+            '''create table if not exists exercise_table(id INTEGER PRIMARY KEY, row_id INTEGER, reps INTEGER, weight INTEGER, user TEXT, exercise TEXT)''')
+        self.run_query(
+            '''create table if not exists pr_table(id INTEGER PRIMARY KEY, date TEXT, max_weight INTEGER, user TEXT, exercise TEXT)''')
         self.db.commit()
-    
-    def run_query(self, query): 
+
+    def run_query(self, query):
         cursor = self.db.cursor()
         cursor.execute(query)
         return cursor.fetchall()
 
-    def get_pr_data(self,user, exercise):
+    def get_pr_data(self, user, exercise):
         query_string = f'''select * from pr_table where user="{user}" and exercise="{exercise}"'''
         results = self.run_query(query_string)
-        print('DB: retrived:',results)
-        df = pd.DataFrame(results, columns=['id','date','max_weight','user','exercise'])
+        # print('DB: retrived:',results)
+        df = pd.DataFrame(results, columns=[
+                          'id', 'date', 'max_weight', 'user', 'exercise'])
         return df
 
-    def insert_pr_data(self,user, exercise, max_weight):
-        res = self.run_query(f'''insert into pr_table(date, max_weight,user,exercise) values ("{datetime.datetime.now().date()}",{max_weight}, "{user}", "{exercise}")''')
+    def insert_pr_data(self, user, exercise, max_weight):
+        res = self.run_query(
+            f'''insert into pr_table(date, max_weight,user,exercise) values ("{datetime.datetime.now().date()}",{max_weight}, "{user}", "{exercise}")''')
         self.db.commit()
-        print(f'DB: inserted values {user} {exercise} {max_weight}' )
+        # print(f'DB: inserted values {user} {exercise} {max_weight}')
         return res
-    
-    # def remove_last_pr_row(self):
-    #     query = '''delete from pr_table where id= (select max(id) from exercises)'''
-    #     res = self.run_query(query)
-    #     self.db.commit()
-    #     return res
-    
+
     def get_exercise_data(self, user, exercise):
         query_string = f'''select * from exercise_table where user="{user}" and exercise="{exercise}" order by row_id asc'''
         # print(query_string)
         res = self.run_query(query_string)
-        print(res)
-        ret = [{'reps_col': i[2], 'weight_col':i[3]} for i in res]
-        print(ret)
+        # print('GET: exercise_table', res)
+        ret = [{'reps_col': i[2], 'weight_col': i[3]} for i in res]
         return ret
-    
-    def insert_exercise_data(self,table_data, user, exercise): #TODO: this is broken lol, nothing gets inserted.
-        #delete all where user and exercise
+
+    def insert_exercise_data(self, table_data, user, exercise):
+        # delete all where user and exercise
         query_string = f'''delete from exercise_table where user="{user}" and exercise="{exercise}"'''
         self.run_query(query_string)
-        #insert 
+        # insert and save highest weight
+        max_weight = 0
         for row in table_data:
-            print('ROW:', row) 
+            # print('ROW:', row)
+            if int(row['weight_col']) > max_weight:
+                max_weight = int(row['weight_col'])
             query_string = f'''insert into exercise_table(reps,weight,row_id,user,exercise) values(
                 {row['reps_col']}, {row['weight_col']},{table_data.index(row)}, "{user}", "{exercise}")'''
-            print(query_string)
+            # print(query_string)
             self.run_query(query_string)
         self.db.commit()
-        
-        # id = hash( str(row_id) + user + exercise)
-        # query = f'''insert into exercise_table (id, reps, weight, row_id, user, exercise) 
-        # values({id}, {reps}, {weight}, {row_id}, "{user}", "{exercise}") 
-        # on conflict(id) 
-        # do update set reps=excluded.reps, weight=excluded.weight '''
-        # print(query)
-        # res = self.run_query(query)
-        # self.db.commit()
-        # return res
-    
-#https://dash.plotly.com/datatable/editable#adding-or-removing-rows
-#Table: exercise:: row-id, reps, weight, user, exercise
-#Table: pr:: date, max_weight, exercise, user
+        # insert max_weight for max graph
+        if max_weight > 0:
+            # print('inserting max weight', max_weight)
+            self.insert_pr_data(user, exercise, max_weight)
 
-#select * from exercise where user = USER and exercise = EXERCISE order by row-id asc
-#on row update: compare if highest value in data is higher than updated value and if so insert into pr... (literally the insert data function we already have.)
+
+# https://dash.plotly.com/datatable/editable#adding-or-removing-rows
+# Table: exercise:: row-id, reps, weight, user, exercise
+# Table: pr:: date, max_weight, exercise, user
+
+# select * from exercise where user = USER and exercise = EXERCISE order by row-id asc
+# on row update: compare if highest value in data is higher than updated value and if so insert into pr... (literally the insert data function we already have.)
--- gymbro.py
+++ gymbro.py
@@ -1,9 +1,7 @@
-from dash import Dash, html, dcc, callback, Output, Input, State, dash_table
+from dash import Dash, html, dcc, callback, Output, Input, State, dash_table, callback_context
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import db_helper
-
-#TODO: save data in dict and majke button to finalize changes
 
 dbh = db_helper.Db_Helper()
 INITIAL_DATA = [{'reps_col': '', 'weight_col': ''}]
@@ -27,19 +25,6 @@ app.layout = [
                    value=EXERCISE, id='exercise_chooser', inline=True),
     dcc.Graph(id='weight_graph', figure={}),
     html.Hr(),
-    # html.Div([
-    #     dbc.Row([
-    #         dbc.Label("Max Weight Today", html_for='max_input'),
-    #         dbc.Col(dcc.Input(id='max_input', type='text',
-    #                 placeholder='Max', value='')),
-    #         dbc.Col(dbc.Button("Enter", id='max_input_btn', color='primary',
-    #                 n_clicks=0))
-    #     ]),
-    #     dbc.Row([
-    #         dbc.Button("Undo Last Entry", id='undo_btn',
-    #                    color='secondary', n_clicks=0)
-    #     ])
-    # ]),
     html.Div([
         dash_table.DataTable(
              id='workout_table',
@@ -51,40 +36,14 @@ app.layout = [
              editable=True,
              row_deletable=True
              ),
-    dbc.Row([
-        html.Hr(),
-        html.Button('Add Row', id='add_row_btn', n_clicks=0),
-        html.Button('Save Data', id='save_data_btn', n_clicks=0)
-    ])
+        dbc.Row([
+            html.Hr(),
+            html.Button('Add Row', id='add_row_btn'),
+            html.Button('Save Data', id='save_data_btn')
+        ])
     ])
 ]
 
-
-
-# @callback(
-#     Output('workout_table', 'data', allow_duplicate=True),
-#     Input('workout_table', 'data_timestamp'),
-#     State('workout_table', 'data'),
-#     State('workout_table', 'columns'), prevent_initial_call=True)
-# def edit_table(timestamp, rows, columns):
-#     print(rows)
-#     if timestamp > TIMESTAMP:
-#         for i in range(0,len(rows)):
-#             reps = rows[i]['reps_col']
-#             weight = rows[i]['weight_col']
-#             row_id = i
-#             if not reps:
-#                 reps = 0
-#             if not weight:
-#                 weight = 0
-
-#     # print('im crying', something)
-#     return rows
-
-
-# def save_table_data(save_clicks, table_data):
-    
-    # return
 
 @callback(
     Output('weight_graph', 'figure'),
@@ -96,31 +55,31 @@ app.layout = [
     State('workout_table', 'data')
 )
 def update_user(user_value, exercise_value, save_clicks, add_row, table_data):
-    print('USER:', user_value)
+    # https://stackoverflow.com/questions/62119605/dash-how-to-callback-depending-on-which-button-is-being-clicked
+    trigger = callback_context.triggered[0]['prop_id'].split('.')[0]
+    # print('TRIGGER',trigger)
     USER = user_value
-    print('EXERCISE', exercise_value)
     EXERCISE = exercise_value
-
+    # print('USER:', user_value)
+    # print('EXERCISE', exercise_value)
     rows = get_table_data(USER, EXERCISE)
-    print('displaying rows', rows)
-    if add_row > 0:
-        rows.extend(INITIAL_DATA)
-        return update_graph(user_value, exercise_value), rows
-    
-    if save_clicks > 0:
-        print('save:', table_data)
-        dbh.insert_exercise_data(table_data,USER, EXERCISE)
-        return update_graph(user_value, exercise_value), table_data
-        # for row in table_data:
-        #     print('ROW:', row) 
-        #     dbh.insert_exercise_data(row['reps_col'], row['weight_col'], table_data.index(row), USER, EXERCISE)
-
-    return update_graph(user_value, exercise_value), rows
+    # print('displaying rows', rows)
+    match trigger:
+        case 'add_row_btn':
+            rows.extend(INITIAL_DATA)
+            return update_graph(user_value, exercise_value), rows
+        case 'save_data_btn':
+            # print('save:', table_data)
+            dbh.insert_exercise_data(table_data, USER, EXERCISE)
+            return update_graph(user_value, exercise_value), table_data
+        case _:
+            return update_graph(user_value, exercise_value), rows
 
 
 def get_table_data(user, exercise):
     df = dbh.get_exercise_data(user, exercise)
     return df
+
 
 def update_graph(user, exercise):
     df = dbh.get_pr_data(user, exercise)
@@ -128,4 +87,4 @@ def update_graph(user, exercise):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
